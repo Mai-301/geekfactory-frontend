60b67b8ddefeeb752372727f4a319c750a9230b0
Mai Sadek <mmsadek97@gmail.com>
2017-02-08 23:38:52 +0200

    implementing track function

--- assignments/assignment11/task.js
+++ assignments/assignment11/task.js
@@ -5,6 +5,13 @@ var Task = function (category, title, priority, estimate) {
     this.estimate = estimate;
     this.spent = 0;
     this.remaining = 1.5;
+    this.track = function (trackNum) {
+        //for track functions assertions
+        if (!isNaN(trackNum) && trackNum > 0) {
+            this.spent = trackNum;
+            this.remaining = this.estimate - this.spent;
+        }
+    }
 
 }
 module.exports = Task;
